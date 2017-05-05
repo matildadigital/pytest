492dcba1c459768b093cb0aa9577e14938c1d78d
matildadigital <techspec@ALD-MASUZU.local>
2017-05-05 14:21:57 -0600

    merged data with csv output

--- fileTransform2.py
+++ fileTransform2.py
@@ -5,8 +5,11 @@ import matplotlib.pyplot as plt
 file1 = 'daily.csv'
 file2 = 'companies.csv'
 finalfile = 'merged.csv'
-start_date = '1/4/17'
+start_date = '1/3/17'
 end_date = '1/8/17'
+
+start_date = pd.to_datetime(start_date)
+end_date = pd.to_datetime(end_date)
 
 daily_df = pd.read_csv(file1)
 print(daily_df)
@@ -15,14 +18,15 @@ companies_df = pd.read_csv(file2)
 print(companies_df)
 
 mergedData = pd.merge(daily_df, companies_df)
-
 print(mergedData.sort_values(by='date'))
 
 mergedData['date'] = pd.to_datetime(mergedData.date)
+mergedData = mergedData[(mergedData['date'] >= start_date) & (mergedData['date'] <= end_date)]
 print(mergedData.sort_values(by='date'))
 
 mergedData['difference'] = mergedData['value'] - mergedData['value'].shift(1)
-print(mergedData.sort_values(['date', 'id'], ascending=[True,True]))
+mergedData = mergedData.sort_values(['date', 'id'], ascending=[True,True])
+print(mergedData)
 
 mergedData.to_csv(finalfile, sep=',', mode='w')
 
